90d89e9159f3ed805ca73a8cb6a18eb00dba9f2b
omercsoylu <omercsoylu@gmail.com>
2023-01-04 18:56:09 +0300

    Week 4 - Task 1 - Build and Deploy an Ether-Store Smart Contract with Remix IDE

--- Contracts/1_FeeCollector.sol
+++ Contracts/1_FeeCollector.sol
@@ -7,21 +7,27 @@ contract FeeCollector {
     uint256 public balance;
 
     constructor() {
+        // When deploying the contract, the wallet address is assigned to the "owner" variable.
         owner = msg.sender;
     }
 
     receive() external payable {
+        // allows the balance of the contract to be updated when it receives "Ether"
         balance += msg.value;
     }
 
     function withdraw(uint256 _amount, address payable _toAddress) public {
+        // checks whether the wallet interacting with the function belongs to the "owner".
         require(
             owner == msg.sender,
             "You are not authorized to this transaction."
         );
+        // checks if there is enough balance to withdraw
         require(balance >= _amount, "Insufficient balance.");
 
+        // we update the "balance" variable before action to avoid any security problems.
         balance -= _amount;
+        // the requested balance is transferred to the destination address.
         _toAddress.transfer(_amount);
     }
 }
